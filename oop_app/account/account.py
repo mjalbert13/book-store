f7322baf96b8d1d5c0d4494c8db3864c07bced67
mjalbert13 <mjalbert13@gmail.com>
2020-02-19 11:57:50 -0500

    practice inheritence

--- oop_app/account/account.py
+++ oop_app/account/account.py
@@ -15,9 +15,17 @@ class Account():
         with open(self.filepath, "w") as file:
             file.write(str(self.balance))
 
-account = Account("ballance.txt")
-print(account.balance)
 
-account.deposit(200)
-print(account.balance)
-account.commit()
+class Checking(Account):
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
+    def transfer(self, amount):
+        self.balance = self.balance -amount- self.fee
+
+
+cheecking = Checking("ballance.txt", 2)
+cheecking.transfer(100)
+print(cheecking.balance)
+cheecking.commit()
